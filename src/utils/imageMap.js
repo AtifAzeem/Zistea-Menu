6c94d37b1c3832ed20aecce26ef60ca7ae138e01
Atif <atifazeem950@gmail.com>
2026-07-23 19:38:31 +0530

    Complete menu data and image mapping

---
+++ src/utils/imageMap.js
@@ -0,0 +1,7 @@
+export const imageMap = {
+    // Coffee
+    "Classic Brew": "/menu/coffee/classic.webp",
+    "Chocolate Frappe": "/menu/coffee/chocolate-frappe.webp",
+
+    // More mappings coming...
+};
