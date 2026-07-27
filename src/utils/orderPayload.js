e01eb559872f4275540e78186e25a189342d1876
Atif <atifazeem950@gmail.com>
2026-07-27 14:17:05 +0530

    qr payload modified

--- src/utils/orderPayload.js
+++ src/utils/orderPayload.js
@@ -6,11 +6,45 @@
 //   };
 // }
 
+// export function createOrderPayload(tableNumber, cartItems) {
+//   const items = cartItems.map((item) => [
+//     item.id,
+//     item.variant.id,
+//     item.addons.map((addon) => addon.id),
+//     item.quantity,
+//   ]);
+
+//   return {
+//     v: 2,
+//     t: tableNumber,
+//     i: items,
+//   };
+// }
+
+const VARIANT_QR = {
+  standard: "st",
+  small: "sm",
+  medium: "md",
+  large: "lg",
+};
+
+const ADDON_QR = {
+  cheese: "c",
+  butter: "b",
+  honey: "h",
+  mayo: "m",
+  ice_cream: "ic",
+  chocolate_sauce: "cs",
+  whipped_cream: "wc",
+};
+
 export function createOrderPayload(tableNumber, cartItems) {
   const items = cartItems.map((item) => [
     item.id,
-    item.variant.id,
-    item.addons.map((addon) => addon.id),
+    VARIANT_QR[item.variant.id] ?? item.variant.id,
+    item.addons.map(
+      (addon) => ADDON_QR[addon.id] ?? addon.id
+    ),
     item.quantity,
   ]);
 
